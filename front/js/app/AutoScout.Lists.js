9aa12aa17a75799cf891cd825b4ef7233158ff86
swizkon <jonas@jerndin.se>
2017-03-23 22:39:46 +0100

    Initial commit with the base

---
+++ front/js/app/AutoScout.Lists.js
@@ -0,0 +1,21 @@
+var AutoScout = window['AutoScout'] || {};
+
+
+AutoScout.List = function(name) {
+    var list = {
+        title: name,
+        timestamp: new Date().getTime(),
+        items: []
+    };
+
+    list.name = function(){
+        return this.title;
+    };
+
+    list.addItem = function(name) {
+        this.items[this.items.length] = {name: name, quant: 1};
+    };
+
+
+    return list;
+};
