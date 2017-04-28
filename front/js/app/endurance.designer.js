3a9c035b1a4c58c7ebae745022d0a950a219b8fd
swizkon <jonas@jerndin.se>
2017-04-28 12:18:05 +0200

    Trimmed some code

--- front/js/app/endurance.designer.js
+++ front/js/app/endurance.designer.js
@@ -9,7 +9,29 @@ endurance.designer = {
             || inpath != context.isPointInStroke(x + 1, y)
             || inpath != context.isPointInStroke(x, y - 1)
             || inpath != context.isPointInStroke(x, y + 1);
+    },
+
+    pointsOfInterest: function (canvas) {
+        var context = canvas.getContext("2d");
+        var x, y, pointsOfInterest = {"on": [], "off": []};
+        for (x = 0; x < canvas.width; x += 1) {
+            for (y = 0; y < canvas.height; y += 1) {
+
+                var inpath = context.isPointInStroke(x, y);
+
+                var type = inpath ? "on" : "off";
+                
+                var isOfInterest = endurance.designer.isPointOfInterest(previewCanvasContext, x, y);
+                if (isOfInterest) {
+                    pointsOfInterest[type][pointsOfInterest[type].length] = {
+                        'x': x,
+                        'y': y
+                    };
+                }
+            }
+        }
+
+        return pointsOfInterest;
     }
 
-    
 };
